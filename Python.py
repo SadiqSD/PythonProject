8e1e6204a3817d97e2c17f57cfa8b94792d64d12
SadiqSD <sadiqmurtala20@gmail.com>
2022-04-19 14:19:48 +0100

    Added a name ask

--- Python.py
+++ Python.py
@@ -4,3 +4,5 @@ from unicodedata import name
 print("one of the most amazing people in the world right know is my dad and my mom")
 name = "sadiq is my faverite name"
 print(name)
+name = input("what's your name? ")
+print(f"You're welcome {name} ")
