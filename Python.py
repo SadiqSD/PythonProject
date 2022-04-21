d999441c25a5367b07bf534bf7738a5f0566fd4d
SadiqSD <sadiqmurtala20@gmail.com>
2022-04-21 11:38:08 +0100

    updated

--- Python.py
+++ Python.py
@@ -1,3 +1,7 @@
 #A leader is ment to surffet but as time goes on all the pain will disappere and joy will soon emerge
 
 #You have to do something that will atract the next follower
+
+#'*functions
+print(444/1)
+# orders of number
