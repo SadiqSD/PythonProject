b3ad3fb3ccbc330bf0b48fa6c877e88339b9efc4
SadiqSD <sadiqmurtala20@gmail.com>
2022-04-22 12:24:57 +0100

    Updtate

--- Python.py
+++ Python.py
@@ -9,24 +9,50 @@
 #was having 20$ and then he spent some so we could simpy activate it buy doing this below
 
 #let's make some people happy
+#
+# import random
+# random_numbers = random.randint(1,200)
+# random = random.randint(1,5)
+# random_text = ""
+#
+# if random == 1:
+#     random_text = f"you're the most butifull person i have ever seen you you smile."
+# if random == 2:
+#     random_text = f"Keep smiling, because life is a beautiful thing and there's so much to smile about."
+# if random == 3:
+#     random_text = f"Life is either a daring adventure or nothing at all. your luck number"
+# if random == 4:
+#         random_text = f"You own your own happiness, so don't let people control you"
+# if random == 5:
+#     random_text = f"Many people will not belive in you but makes sure you belive in you"
+#
+# print(f"{random_text} your lucy number is : {random_numbers}")
+#
+#
 
-import random
-random_numbers = random.randint(1,200)
-random = random.randint(1,5)
-random_text = ""
 
-if random == 1:
-    random_text = f"you're the most butifull person i have ever seen you you smile."
-if random == 2:
-    random_text = f"Keep smiling, because life is a beautiful thing and there's so much to smile about."
-if random == 3:
-    random_text = f"Life is either a daring adventure or nothing at all. your luck number"
-if random == 4:
-        random_text = f"You own your own happiness, so don't let people control you"
-if random == 5:
-    random_text = f"Many people will not belive in you but makes sure you belive in you"
+string = '23 youre amazing'
+print(string.isdigit())
 
-print(f"{random_text} your lucy number is : {random_numbers}")
+
+fev_movie = ["ironman","spiderman","superman","Dune"]
+
+
+del(fev_movie[0])
+del(fev_movie[0])
+del(fev_movie[0])
+
+
+
+#changing things inside list
+
+
+
+
+print(fev_movie)
+
+
+
 
 
 
