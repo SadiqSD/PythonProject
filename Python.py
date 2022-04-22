d5adf231164c58051850505536af7b487a0a02ce
SadiqSD <sadiqmurtala20@gmail.com>
2022-04-22 09:47:57 +0100

    Update from yesteday

--- Python.py
+++ Python.py
@@ -21,6 +21,11 @@ if random == 2:
     random_text = f"Keep smiling, because life is a beautiful thing and there's so much to smile about."
 if random == 3:
     random_text = f"Life is either a daring adventure or nothing at all. your luck number"
+if random == 4:
+        random_text = f"You own your own happiness, so don't let people control you"
+if random == 5:
+    random_text = f"Many people will not belive in you but makes sure you belive in you"
+
 print(f"{random_text} your lucy number is : {random_numbers}")
 
 
@@ -29,7 +34,7 @@ print(f"{random_text} your lucy number is : {random_numbers}")
 
 #will be updated later
 
-
+#meaning of the lucky numbers
 
 
 
